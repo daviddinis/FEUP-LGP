00a25e743c519281cc0da21ea1f2fe8223217e39
Mariana Neto <32868723+bluediberry@users.noreply.github.com>
2021-04-10 17:05:27 +0100

    Some organization

---
+++ frontend/src/components/Sidebar/Sidebar.tsx
@@ -0,0 +1,47 @@
+import React from "react";
+import '@trendmicro/react-sidenav/dist/react-sidenav.css';
+import './Sidebar.scss';
+import SideNav, { Toggle, Nav, NavItem, NavIcon, NavText } from '@trendmicro/react-sidenav';
+
+
+const Sidebar = () => {
+
+    return (
+    <SideNav className="sidebar"
+    onSelect={(selected) => {
+        // Add your code here
+    }}
+>
+    <SideNav.Toggle />
+    <SideNav.Nav className="sidebar" defaultSelected="home">
+        <NavItem eventKey="documents" className="navitem">
+            <NavIcon>
+                <i className="fa fa-fw documents" style={{ fontSize: '1.75em' }} />
+            </NavIcon>
+            <NavText className="text">
+                Submited Documents
+            </NavText>
+        </NavItem>
+        <NavItem eventKey="parameters" className="navitem">
+            <NavIcon>
+                <i className="fa fa-fw parameters " style={{ fontSize: '1.75em' }} />
+            </NavIcon>
+            <NavText className="text">
+                Parameters / types
+            </NavText>
+        </NavItem>
+        <NavItem eventKey="users" className="navitem">
+            <NavIcon>
+                <i className="fa fa-fw users" style={{ fontSize: '1.75em' }} />
+            </NavIcon>
+            <NavText className="text">
+                Registered Users
+            </NavText>
+        </NavItem>
+    </SideNav.Nav>
+</SideNav>
+    )
+}
+
+export default Sidebar;
+
--- frontend/src/pages/AdminFeed/AdminFeed.tsx
+++ frontend/src/pages/AdminFeed/AdminFeed.tsx
@@ -1,13 +1,10 @@
 import React, { useEffect, useState } from 'react';
-import '@trendmicro/react-sidenav/dist/react-sidenav.css';
 import '../Table.scss';
 import './AdminFeed.scss';
 import axios from "axios";
 import SubmitionLine from '../../components/SubmitionLine';
 import Header from '../../components/Header/Header';
-import SideNav, { Toggle, Nav, NavItem, NavIcon, NavText } from '@trendmicro/react-sidenav';
-
-
+import Sidebar from '../../components/Sidebar/Sidebar';
 
 interface IUser {
   username: string
@@ -31,40 +28,8 @@ function App() {
     <div className="Admin-Page">
       <Header username="gingerAle" isAdmin={ true }/>
 
-            <SideNav className="sidebar"
-          onSelect={(selected) => {
-              // Add your code here
-          }}
-      >
-          <SideNav.Toggle />
-          <SideNav.Nav className="sidebar" defaultSelected="home">
-              <NavItem eventKey="documents" className="navitem">
-                  <NavIcon className="navitem">
-                      <i className="fa fa-fw documents" style={{ fontSize: '1.75em' }} />
-                  </NavIcon>
-                  <NavText className="text">
-                      Submited Documents
-                  </NavText>
-              </NavItem>
-              <NavItem eventKey="parameters">
-                  <NavIcon>
-                      <i className="fa fa-fw parameters " style={{ fontSize: '1.75em' }} />
-                  </NavIcon>
-                  <NavText>
-                      Parameters / types
-                  </NavText>
-              </NavItem>
-              <NavItem eventKey="users">
-                  <NavIcon>
-                      <i className="fa fa-fw users" style={{ fontSize: '1.75em' }} />
-                  </NavIcon>
-                  <NavText>
-                      Registered Users
-                  </NavText>
-              </NavItem>
-          </SideNav.Nav>
-      </SideNav>
-
+      <Sidebar/>   
+         
       <div className="content">
       <table className={'submitions'}>
       <tr className={'column-names'}>
