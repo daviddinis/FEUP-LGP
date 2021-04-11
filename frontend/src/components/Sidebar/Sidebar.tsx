ae2a927774e0fd0ce5ced7f899d232214263e501
Mariana Neto <32868723+bluediberry@users.noreply.github.com>
2021-04-11 15:20:28 +0100

    Update on sidebar

--- frontend/src/components/Sidebar/Sidebar.tsx
+++ frontend/src/components/Sidebar/Sidebar.tsx
@@ -1,47 +1,52 @@
-import React from "react";
-import '@trendmicro/react-sidenav/dist/react-sidenav.css';
+import React, { useState } from 'react';
+import * as FaIcons from 'react-icons/fa';
+import * as AiIcons from 'react-icons/ai';
+import { BrowserRouter, Link } from 'react-router-dom';
+import { SidebarData } from './SidebarData.js';
 import './Sidebar.scss';
-import SideNav, { Toggle, Nav, NavItem, NavIcon, NavText } from '@trendmicro/react-sidenav';
+import { IconContext } from 'react-icons';
 
+function Sidebar() {
+  const [sidebar, setSidebar] = useState(false);
 
-const Sidebar = () => {
+  const showSidebar = () => setSidebar(!sidebar);
 
-    return (
-    <SideNav className="sidebar"
-    onSelect={(selected) => {
-        // Add your code here
-    }}
->
-    <SideNav.Toggle />
-    <SideNav.Nav className="sidebar" defaultSelected="home">
-        <NavItem eventKey="documents" className="navitem">
-            <NavIcon>
-                <i className="fa fa-fw documents" style={{ fontSize: '1.75em' }} />
-            </NavIcon>
-            <NavText className="text">
-             Submitted Documents           
-            </NavText>
-        </NavItem>
-        <NavItem eventKey="parameters" className="navitem">
-            <NavIcon>
-                <i className="fa fa-fw parameters " style={{ fontSize: '1.75em' }} />
-            </NavIcon>
-            <NavText className="text">
-                Parameters / types
-            </NavText>
-        </NavItem>
-        <NavItem eventKey="users" className="navitem">
-            <NavIcon>
-                <i className="fa fa-fw users" style={{ fontSize: '1.75em' }} />
-            </NavIcon>
-            <NavText className="text">
-                Registered Users
-            </NavText>
-        </NavItem>
-    </SideNav.Nav>
-</SideNav>
-    )
+  return (
+    <>
+      <IconContext.Provider value={{ color: '#4AC4F4' }}>
+        <div className='navbar'>
+        <BrowserRouter>
+          <Link to='#' className='menu-bars'>
+            <FaIcons.FaBars onClick={showSidebar} />
+          </Link>
+          </BrowserRouter>
+        </div>
+        <nav className={sidebar ? 'nav-menu active' : 'nav-menu'}>
+          <ul className='nav-menu-items' onClick={showSidebar}>
+            <li className='navbar-toggle'>
+            <BrowserRouter>
+              <Link to='#' className='menu-bars'>
+                <AiIcons.AiOutlineClose />
+              </Link>
+              </BrowserRouter>
+            </li>
+            {SidebarData.map((item, index) => {
+              return (
+                <li key={index} className={item.cName}>
+                  <BrowserRouter>
+                  <Link to={item.path}>
+                    {item.icon}
+                    <span>{item.title}</span>
+                  </Link>
+                  </BrowserRouter>
+                </li>
+              );
+            })}
+          </ul>
+        </nav>
+      </IconContext.Provider>
+    </>
+  );
 }
 
 export default Sidebar;
-
---
+++ frontend/src/components/Sidebar/SidebarData.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+export const SidebarData = [
+  {
+    title: 'Submitted Documents',
+    path: '/',
+    cName: 'nav-text'
+  },
+  {
+    title: 'Parameters / types',
+    path: '/',
+    cName: 'nav-text'
+  },
+  {
+    title: 'Registered Users',
+    path: '/',
+    cName: 'nav-text'
+  },
+];
