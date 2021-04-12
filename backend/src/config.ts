d329213dc41ff51b6dc2dc9cfca07f0a9100686c
MOAAS <miguel.moas@gmail.com>
2021-04-12 15:24:16 +0100

    ci: fix pipeline errors
    test eu já apago este commit ninguem vai descobrir
    
    test eu já apago este commit ninguem vai descobrir
    
    test eu já apago este commit ninguem vai descobrir
    
    test eu já apago este commit ninguem vai descobrir
    
    test eu já apago este commit ninguem vai descobrir
    
    test eu já apago este commit ninguem vai descobrir
    
    test eu já apago este commit ninguem vai descobrir
    
    test eu já apago este commit ninguem vai descobrir
    
    test eu já apago este commit ninguem vai descobrir

--- backend/src/config.ts
+++ backend/src/config.ts
@@ -6,7 +6,6 @@ export default {
   environment: process.env.NODE_ENV || 'development',
   port: process.env.PORT || 3001,
   loggerLevel: process.env.LOGGER_LEVEL || 'dev',
-  appUrl: process.env.APP_URL,
   mongoUrl: process.env.DB_URL || 'mongodb://localhost:27017',
   docparserApiKey: process.env.DOCPARSER_API_KEY,
   docparserParserId: process.env.DOCPARSER_PARSERID
