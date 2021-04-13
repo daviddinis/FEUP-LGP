6832436d0e57165607be9f16e2014f10113fac2f
Filipa Senra <filipasenra4@gmail.com>
2021-04-13 17:44:37 +0100

    Running prettier

--- frontend/src/pages/AdminFeed/AdminFeed.tsx
+++ frontend/src/pages/AdminFeed/AdminFeed.tsx
@@ -1,68 +1,100 @@
-import React, { useEffect, useState } from 'react';
-import '../Table.scss';
+import React, { useEffect, useState } from "react";
+import "../Table.scss";
 import axios from "axios";
-import SubmitionLine from '../../components/SubmitionLine';
-import Header from '../../components/Header/Header';
-import Sidebar from '../../components/Sidebar/Sidebar';
+import SubmitionLine from "../../components/SubmitionLine";
+import Header from "../../components/Header/Header";
+import Sidebar from "../../components/Sidebar/Sidebar";
 
 interface File {
-  _id: string,
-  path: string,
-  name: string
+  _id: string;
+  path: string;
+  name: string;
 }
 
 function AdminFeed() {
   const [files, setUsers] = useState<File[]>([]);
 
   useEffect(() => {
-    axios.get("/files").then(res => {      
+    axios.get("/files").then((res) => {
       setUsers(res.data);
       console.log(res.data);
-    })
+    });
   }, []);
 
+  const submitions: any[] = [
+    {
+      id: "1",
+      isFlaged: true,
+      user: "filipasenra",
+      documentName: "anualreportdocura",
+      type: "extract",
+      format: "pdf",
+      date: "25-03-2021",
+    },
+    {
+      id: "2",
+      isFlaged: false,
+      user: "claudiasilva",
+      documentName: "IDClaudia",
+      type: "pdf",
+      format: "jpeg",
+      date: "21-03-2021",
+    },
+  ];
 
-  const submitions: any[] = [{id: "1", isFlaged: true, user: "filipasenra", documentName: "anualreportdocura", type: "extract", format: "pdf", date: "25-03-2021",},
-  {id: "2", isFlaged: false, user: "claudiasilva", documentName: "IDClaudia", type: "pdf", format: "jpeg", date: "21-03-2021",}];
-
-  for(let i = 0; i != files.length; i++) {
+  for (let i = 0; i != files.length; i++) {
     const id = files[i]._id;
     const name = files[i].name;
 
-    submitions.push({id: id, isFlaged: false, user: "pc", documentName: name, type: "extract", format: "pdf", date: "30-01-2012"});
+    submitions.push({
+      id: id,
+      isFlaged: false,
+      user: "pc",
+      documentName: name,
+      type: "extract",
+      format: "pdf",
+      date: "30-01-2012",
+    });
 
     console.log(submitions);
   }
 
   return (
-
     <div className="admin-feed">
-      <Header username="gingerAle" isAdmin={ true }/>
+      <Header username="gingerAle" isAdmin={true} />
 
-      <Sidebar/>   
-         
+      <Sidebar />
+
       <div className="content">
-      <table className={'submitions'}>
-      <thead>
-        <tr>
-          <th></th>
-          <th>name</th>
-          <th>name</th>
-          <th>type</th>
-          <th>format</th>
-          <th>date</th>
-        </tr>
-      </thead>
-      
-      <tbody>
-        {submitions.map(submition => { return (
-          <SubmitionLine key={submition.id} isFlaged={submition.isFlaged} user={submition.user} documentName={submition.documentName} type={submition.type} format={submition.format} date={submition.date}/>
-        )})}
-      </tbody>
+        <table className={"submitions"}>
+          <thead>
+            <tr>
+              <th></th>
+              <th>name</th>
+              <th>name</th>
+              <th>type</th>
+              <th>format</th>
+              <th>date</th>
+            </tr>
+          </thead>
 
-    </table>
-
-    </div>
+          <tbody>
+            {submitions.map((submition) => {
+              return (
+                <SubmitionLine
+                  key={submition.id}
+                  isFlaged={submition.isFlaged}
+                  user={submition.user}
+                  documentName={submition.documentName}
+                  type={submition.type}
+                  format={submition.format}
+                  date={submition.date}
+                />
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 }
--- frontend/src/pages/UserFeed/UserFeed.tsx
+++ frontend/src/pages/UserFeed/UserFeed.tsx
@@ -1,72 +1,76 @@
-import React, { ReactNode, useEffect, useState, useCallback } from 'react';
-import { useDropzone } from 'react-dropzone';
+import React, { ReactNode, useEffect, useState, useCallback } from "react";
+import { useDropzone } from "react-dropzone";
 
 import Header from "../../components/Header/Header";
-import './UserFeed.scss';
-import '../Table.scss';
-import '../../index.css'
+import "./UserFeed.scss";
+import "../Table.scss";
+import "../../index.css";
 import axios from "axios";
-import document from '../../shared/icons/document.svg';
-import SubmitionLineUser from '../../components/SubmitionLineUser';
+import document from "../../shared/icons/document.svg";
+import SubmitionLineUser from "../../components/SubmitionLineUser";
 
-function UserPage() {
-
-  const onDrop = useCallback(acceptedFiles => {
-
+function UserFeed() {
+  
+  const onDrop = useCallback((acceptedFiles) => {
     const formData = new FormData();
-    formData.append('file', acceptedFiles[0]);
+    formData.append("file", acceptedFiles[0]);
     const config = {
       headers: {
-        'content-type': 'multipart/form-data'
-      }
+        "content-type": "multipart/form-data",
+      },
     };
 
     axios.post("/sendFile", formData, config);
-  }, [])
+  }, []);
 
-  const { acceptedFiles, getRootProps, getInputProps } = useDropzone({ onDrop });
+  const { acceptedFiles, getRootProps, getInputProps } = useDropzone({
+    onDrop,
+  });
 
   return (
     <div className="user-page">
-      <header className={'header'}>
-        <Header
-          username="gingerAle"
-          isAdmin={false} />
-        <div {...getRootProps({ className: 'dropzone' })}>
+      <header className={"header"}>
+        <Header username="gingerAle" isAdmin={false} />
+
+        <div {...getRootProps({ className: "dropzone" })}>
           <input {...getInputProps()} />
-          <img
-            className={'icon document'}
-            src={document} />
-          <label className={'drop-file-label'}><strong>choose a file</strong> or drag it here.</label>
+          <img className={"icon document"} src={document} />
+          <label className={"drop-file-label"}>
+            <strong>choose a file</strong> or drag it here.
+          </label>
         </div>
       </header>
 
       <div className="content">
-      <table className={'submitions'}>
-      <thead>
-        <tr>
-          <th>status</th>
-          <th></th>
-          <th>name</th>
-          <th>type</th>
-          <th>format</th>
-          <th>date</th>
-          <th></th>
-        </tr>
-      </thead>
+        <table className={"submitions"}>
+          <thead>
+            <tr>
+              <th>status</th>
+              <th></th>
+              <th>name</th>
+              <th>type</th>
+              <th>format</th>
+              <th>date</th>
+              <th></th>
+            </tr>
+          </thead>
 
-      <tbody>
-
-        {acceptedFiles.map(submition => { return (
-          <SubmitionLineUser key={1} name={submition.name} type={submition.type} date={(new Date(submition.lastModified)).toLocaleDateString()}/>
-        )})}
-      </tbody>
-
-      </table>
-
+          <tbody>
+            {acceptedFiles.map((submition) => {
+              return (
+                <SubmitionLineUser
+                  key={1}
+                  name={submition.name}
+                  type={submition.type}
+                  date={new Date(submition.lastModified).toLocaleDateString()}  //TODO: this date shouldn't be of the file, but of the submission
+                />
+              );
+            })}
+          </tbody>
+        </table>
       </div>
     </div>
   );
 }
 
-export default UserPage;
+export default UserFeed;
