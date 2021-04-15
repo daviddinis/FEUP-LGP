82c0f6823346c0324ce070359cde858f8e5000aa
TigasMiller09 <48095097+TigasMiller09@users.noreply.github.com>
2021-04-15 13:28:38 +0100

    Merge dev

---
+++ backend/frontend/src/components/BottomCornerImage/BottomCornerImage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import cornerImage from '../shared/icons/corner.png';
+import './BottomCornerImage.scss';
+
+
+const Component = () => {
+   return (
+        <div style={{backgroundImage: "url(" + cornerImage + ")"}} className={'bottom-corner-image'}></div>
+    );
+}
+
+export default Component;
