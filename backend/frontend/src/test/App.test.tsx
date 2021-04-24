938f8ea2eadecda36a9612e97fbad997514fdf2b
David Dinis <up201706766@fe.up.pt>
2021-04-24 18:17:56 +0100

    fix: test

--- backend/frontend/src/test/App.test.tsx
+++ backend/frontend/src/test/App.test.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import AdminUserDocs from '../pages/AdminUserDocs/AdminUserDocs';
+import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
 
 test('renders app name', () => {
-  render(<AdminUserDocs />);
+  render(<Router><AdminUserDocs /></Router>);
   const title = screen.getByText(/Know your customer on the news/i);
   expect(title).toBeInTheDocument();
 });
