6af28197d5fe01a516df26748a1489501c763679
TigasMiller09 <48095097+TigasMiller09@users.noreply.github.com>
2021-04-27 12:57:49 +0100

    feat: create popup

--- backend/frontend/src/pages/Parameters/Parameters.tsx
+++ backend/frontend/src/pages/Parameters/Parameters.tsx
@@ -1,20 +1,18 @@
-import React, {useEffect, useState} from "react";
+import React, {} from "react";
 import "./Parameters.scss";
 import Header from "components/Header/Header";
 import addType from "shared/icons/addtype_1.svg";
-
-// import BottomCornerImage from "components/BottomCornerImage/BottomCornerImage";
-
 
 function Parameters(): JSX.Element {
 
   return (
     <div className="add-type-page">
+        
         <div className="add-type-modal">
             <div className="add-type-popup">
-                <p>Add new type</p>
-                <p>Name</p>
-                <input type="text"/>
+                <p className="add-type-title">Add new type</p>
+                <p className="add-type-label">Name</p>
+                <input className="add-type-input" type="text"/>
                 <input className="add-type-submit" type="submit" value="Add"/>
             </div>
       </div>
