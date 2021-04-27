010f60901986fe85629140823d5b9c38417cdf63
TigasMiller09 <48095097+TigasMiller09@users.noreply.github.com>
2021-04-27 12:31:03 +0100

    feat: add type button

--- backend/frontend/src/pages/Parameters/Parameters.tsx
+++ backend/frontend/src/pages/Parameters/Parameters.tsx
@@ -1,20 +1,29 @@
 import React, {useEffect, useState} from "react";
 import "./Parameters.scss";
 import Header from "components/Header/Header";
+import addType from "shared/icons/addtype_1.svg";
+
 // import BottomCornerImage from "components/BottomCornerImage/BottomCornerImage";
 
 
 function Parameters(): JSX.Element {
 
   return (
-    <div className="registered-users-page">
+    <div className="add-type-page">
+        <div className="add-type-modal">
+            <div className="add-type-popup">
+                <p>Add new type</p>
+                <p>Name</p>
+                <input type="text"/>
+                <input className="add-type-submit" type="submit" value="Add"/>
+            </div>
+      </div>
       <Header username="MillerGinger" isAdmin={true} />
       <div className="body-container">
         <p>ola</p>
-        
       </div>
-      <div className="ola">
-        <p>ola</p>
+      <div className="add-type-button">
+        <button><img src={addType} className="add-type-image" /></button>
       </div>
       {/* <BottomCornerImage /> */}
     </div>
