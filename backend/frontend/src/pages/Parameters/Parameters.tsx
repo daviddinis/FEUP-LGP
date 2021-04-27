58056e9f1351465e8556b1772efe4cd262d7a08d
TigasMiller09 <48095097+TigasMiller09@users.noreply.github.com>
2021-04-27 13:23:14 +0100

    feat: add modal functionality

--- backend/frontend/src/pages/Parameters/Parameters.tsx
+++ backend/frontend/src/pages/Parameters/Parameters.tsx
@@ -2,26 +2,48 @@ import React, {} from "react";
 import "./Parameters.scss";
 import Header from "components/Header/Header";
 import addType from "shared/icons/addtype_1.svg";
+import Modal from '@material-ui/core/Modal';
 
 function Parameters(): JSX.Element {
 
+    const body = (
+        <div className="add-type-popup">
+            <p className="add-type-title">Add new type</p>
+            <p className="add-type-label">Name</p>
+            <input className="add-type-input" type="text"/>
+            <input className="add-type-submit" type="submit" value="Add"/>
+        </div>
+    )
+
+    const [open, setOpen] = React.useState(false);
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
   return (
     <div className="add-type-page">
-        
+        <Modal
+            open={open}
+            onClose={handleClose}
+            aria-labelledby="simple-modal-title"
+            aria-describedby="simple-modal-description"
+            className="add-type-modal">
+            {body}
+        </Modal>
         <div className="add-type-modal">
-            <div className="add-type-popup">
-                <p className="add-type-title">Add new type</p>
-                <p className="add-type-label">Name</p>
-                <input className="add-type-input" type="text"/>
-                <input className="add-type-submit" type="submit" value="Add"/>
-            </div>
+            
       </div>
       <Header username="MillerGinger" isAdmin={true} />
       <div className="body-container">
         <p>ola</p>
       </div>
       <div className="add-type-button">
-        <button><img src={addType} className="add-type-image" /></button>
+        <button onClick={handleOpen}><img src={addType} className="add-type-image" /></button>
       </div>
       {/* <BottomCornerImage /> */}
     </div>
