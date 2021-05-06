4fba10c1b754806504233b87dbf99fd62c8d87c9
Ricardo Pinto <ricardofpinto98@gmail.com>
2021-05-06 18:05:57 +0100

    Add the image and fix some minor details

--- backend/frontend/src/pages/HomePage/HomePage.tsx
+++ backend/frontend/src/pages/HomePage/HomePage.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "pages/HomePage/HomePage.scss";
+import map from 'shared/images/mapa.svg';
 
 function AdminFeed(): JSX.Element {
   return (
@@ -9,12 +10,13 @@ function AdminFeed(): JSX.Element {
         <button className="sign-up">Sign Up</button>
       </header>
       <body className="home-body">
-        <h1>Know your costumer on the news</h1>
-        <p>The tool that recognizes and validates sections in a<br/>document and highlights important information.</p>
-        <p>Relieves the burden from an entity and<br/>make the process easier to the client.</p>
+        <h1 className="home-title">Know your costumer on the news</h1>
+        <p className="home-text">The tool that recognizes and validates sections in a<br/>document and highlights important information.</p>
+        <p className="home-text">Relieves the burden from an entity and<br/>make the process easier to the client.</p>
         <div className = "emailArea">
             <input type="text" className="emailInput" placeholder="Email"></input>
             <button className="emailButton">Sign up</button>
+            <img className={ 'home-map' } src={ map }/>
         </div>
       </body>
     </div>
