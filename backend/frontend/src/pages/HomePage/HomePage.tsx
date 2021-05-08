823aa8a52c386470a7d840ffabdeb1ab7ed9af1e
Ricardo Pinto <ricardofpinto98@gmail.com>
2021-05-08 13:30:53 +0100

    Minor fixes sugested in the review

--- backend/frontend/src/pages/HomePage/HomePage.tsx
+++ backend/frontend/src/pages/HomePage/HomePage.tsx
@@ -13,9 +13,9 @@ function AdminFeed(): JSX.Element {
         <h1 className="home-title">Know your costumer on the news</h1>
         <p className="home-text">The tool that recognizes and validates sections in a<br/>document and highlights important information.</p>
         <p className="home-text">Relieves the burden from an entity and<br/>make the process easier to the client.</p>
-        <div className = "emailArea">
-            <input type="text" className="emailInput" placeholder="Email"></input>
-            <button className="emailButton">Sign up</button>
+        <div className = "email-area">
+            <input type="text" className="email-input" placeholder="Email"></input>
+            <button className="email-button">Sign up</button>
             <img className={ 'home-map' } src={ map }/>
         </div>
       </body>
